477edbff05f710fc441ae658729fc9c12f12b5a3
Ruben Garat <ruben.garat@gemserk.com>
2010-02-23 00:18:40 +0000

    added test for http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6302954
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@62 69e4d4fc-dda9-4998-a343-e80dfd0be058

---
+++ core/src/test/java/com/gemserk/componentsengine/properties/PropertiesTest.java
@@ -0,0 +1,25 @@
+package com.gemserk.componentsengine.properties;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.gemserk.componentsengine.entities.Entity;
+
+public class PropertiesTest {
+
+	
+	@Test
+	public void genericFailureTest(){
+		Entity entity = new Entity("id");
+		
+		float value = 123f;
+		String key = "test";
+		entity.addProperty(key, new SimpleProperty<Object>(value));
+		Float valueTest = Properties.getValue(entity, key);
+		assertThat(value, equalTo(valueTest));
+		
+	}
+}
+
