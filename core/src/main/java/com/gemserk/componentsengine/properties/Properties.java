5b4f9dcb61bb22199117493a95e030ec9d94623c
Ruben Garat <ruben.garat@gemserk.com>
2010-02-23 00:05:06 +0000

    attempt to workaround this bug http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6302954
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@60 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- core/src/main/java/com/gemserk/componentsengine/properties/Properties.java
+++ core/src/main/java/com/gemserk/componentsengine/properties/Properties.java
@@ -10,9 +10,8 @@ public class Properties {
 		return new PropertyLocator<T>(prefix + "." + key);
 	}
 
-	@SuppressWarnings("unchecked")
 	public static <T> T getValue(PropertiesHolder propertiesHolder, String key) {
-		return (T) Properties.<Object>property(key).getValue(propertiesHolder);//HACK TO FIX BUG: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6302954
+		return (T) (new PropertyLocator<T>(key)).getValue(propertiesHolder);//HACK TO FIX BUG: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6302954
 	}
 
 	public static void setValue(PropertiesHolder propertiesHolder, String key, Object value) {
