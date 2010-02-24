f1e21181ac25365ca7d0664af841553886e44efc
Ruben Garat <ruben.garat@gemserk.com>
2010-02-24 01:40:54 +0000

    added injection to delegated component
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@68 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- commons/src/main/java/com/gemserk/componentsengine/commons/components/DisablerComponent.java
+++ commons/src/main/java/com/gemserk/componentsengine/commons/components/DisablerComponent.java
@@ -5,12 +5,16 @@ import com.gemserk.componentsengine.entities.Entity;
 import com.gemserk.componentsengine.messages.Message;
 import com.gemserk.componentsengine.properties.Properties;
 import com.gemserk.componentsengine.properties.PropertyLocator;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 
 public class DisablerComponent extends Component {
 
 	private PropertyLocator<Boolean> enabledProperty;
 	
 	private final Component component;
+	
+	@Inject Injector injector;
 
 	public DisablerComponent(Component component) {
 		super(component.getId());
@@ -21,6 +25,7 @@ public class DisablerComponent extends Component {
 	@Override
 	public void onAdd(Entity entity) {
 		super.onAdd(entity);
+		injector.injectMembers(component);
 		component.onAdd(entity);
 	}
 
