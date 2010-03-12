181d9438362cc8e614cd5eda6a33b68fb66ec67d
Ruben Garat <ruben.garat@gemserk.com>
2010-03-12 18:11:47 +0000

    changed followpathcomponent to change direction of velocity to point to the next point in the path, instead of using forces
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@91 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- commons/src/main/java/com/gemserk/componentsengine/commons/components/FollowPathComponent.java
+++ commons/src/main/java/com/gemserk/componentsengine/commons/components/FollowPathComponent.java
@@ -8,6 +8,7 @@ import com.gemserk.componentsengine.entities.Root;
 import com.gemserk.componentsengine.messages.UpdateMessage;
 import com.gemserk.componentsengine.properties.Properties;
 import com.gemserk.componentsengine.properties.PropertyLocator;
+import com.gemserk.componentsengine.utils.AngleUtils;
 import com.google.inject.Inject;
 
 public class FollowPathComponent extends ReflectionComponent {
@@ -22,6 +23,8 @@ public class FollowPathComponent extends ReflectionComponent {
 	PropertyLocator<Vector2f> forceProperty;
 	
 	PropertyLocator<Vector2f> positionProperty;
+
+	private AngleUtils angleUtils = new AngleUtils();
 
 	public FollowPathComponent(String id) {
 		super(id);
@@ -44,10 +47,14 @@ public class FollowPathComponent extends ReflectionComponent {
 		Vector2f nextPosition = path.getPoint(nextPathIndex);
 
 		Vector2f direction = nextPosition.copy().sub(position).normalise();
-		Vector2f currentForce = forceProperty.getValue(entity).copy();
+		Vector2f velocity = Properties.getValue(entity, "movement.velocity");
+		
+		velocity.add(angleUtils.minimumDifference(velocity.getTheta(), direction.getTheta()));
+		
+		//Vector2f currentForce = forceProperty.getValue(entity).copy();
 
-		currentForce.add(direction.scale(1000f));
+		//currentForce.add(direction.scale(1000f));
 
-		forceProperty.setValue(entity, currentForce);
+		
 	}
 }
