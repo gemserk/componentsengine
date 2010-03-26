6abbdc54458865645ddadef62180a5a8cf6707e9
Ariel Coppes <ariel.coppes@gemserk.com>
2010-03-26 19:20:14 +0000

    changed WeaponComponent to trigger a Trigger instead instantiate an entity and send a message to add it
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@109 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- commons/src/main/java/com/gemserk/componentsengine/commons/components/WeaponComponent.java
+++ commons/src/main/java/com/gemserk/componentsengine/commons/components/WeaponComponent.java
@@ -2,23 +2,17 @@ package com.gemserk.componentsengine.commons.components;
 
 import static com.gemserk.componentsengine.properties.Properties.property;
 
-import java.util.Map;
-
 import org.newdawn.slick.geom.Vector2f;
 
 import com.gemserk.componentsengine.components.ReflectionComponent;
 import com.gemserk.componentsengine.entities.Entity;
-import com.gemserk.componentsengine.genericproviders.GenericProvider;
-import com.gemserk.componentsengine.messages.*;
+import com.gemserk.componentsengine.messages.UpdateMessage;
 import com.gemserk.componentsengine.properties.PropertyLocator;
-import com.gemserk.componentsengine.templates.*;
-import com.google.inject.Inject;
+import com.gemserk.componentsengine.triggers.Trigger;
 
 public class WeaponComponent extends ReflectionComponent {
 
 	PropertyLocator<Entity> targetEntityProperty;
-
-	TemplateProvider templateProvider;
 
 	PropertyLocator<Vector2f> positionProperty;
 
@@ -26,33 +20,15 @@ public class WeaponComponent extends ReflectionComponent {
 
 	PropertyLocator<Integer> currentReloadTimeProperty;
 
-	PropertyLocator<String> templateProperty;
-
-	PropertyLocator<GenericProvider> instanceParametersProviderProperty;
-
-	PropertyLocator<Entity> entityProperty;
-
-	MessageQueue messageQueue;
-
-	@Inject
-	public void setMessageQueue(MessageQueue messageQueue) {
-		this.messageQueue = messageQueue;
-	}
-
-	@Inject
-	public void setTemplateProvider(TemplateProvider templateProvider) {
-		this.templateProvider = templateProvider;
-	}
-
+	PropertyLocator<Trigger> triggerProperty;
+	
 	public WeaponComponent(String id) {
 		super(id);
 		targetEntityProperty = property(id, "targetEntity");
 		positionProperty = property(id, "position");
 		reloadTimeProperty = property(id, "reloadTime");
 		currentReloadTimeProperty = property(id, "currentReloadTime");
-		templateProperty = property(id, "template");
-		instanceParametersProviderProperty = property(id, "instanceParameters");
-		entityProperty = property(id, "entity");
+		triggerProperty = property(id, "trigger");
 	}
 
 	@Override
@@ -75,16 +51,9 @@ public class WeaponComponent extends ReflectionComponent {
 		if (targetEntity == null)
 			return;
 
-		String templateName = templateProperty.getValue(entity);
-
-		EntityTemplate bulletTemplate = templateProvider.getTemplate(templateName);
-
-		Map<String, Object> instanceParameters = instanceParametersProviderProperty.getValue(entity).get(entity);
-
-		Entity bullet = bulletTemplate.instantiate("", instanceParameters);
-
-		messageQueue.enqueue(ChildrenManagementMessageFactory.addEntity(bullet, entityProperty.getValue(entity).getId()));
-
+		Trigger trigger = triggerProperty.getValue(entity);
+		trigger.trigger(entity);
+		
 		currentReloadTime = reloadTimeProperty.getValue(entity);
 		currentReloadTimeProperty.setValue(entity, currentReloadTime);
 	}
