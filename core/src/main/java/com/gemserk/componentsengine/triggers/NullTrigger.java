fe36f985c3509df75953695b0e1d84084169b6fb
Ariel Coppes <ariel.coppes@gemserk.com>
2010-03-30 01:02:42 +0000

    added null implementation of Trigger interface
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@112 69e4d4fc-dda9-4998-a343-e80dfd0be058

---
+++ core/src/main/java/com/gemserk/componentsengine/triggers/NullTrigger.java
@@ -0,0 +1,15 @@
+package com.gemserk.componentsengine.triggers;
+
+/**
+ * @author gemserk Null implementation of Trigger.
+ */
+public class NullTrigger implements Trigger {
+	@Override
+	public void trigger() {
+	}
+
+	@Override
+	public void trigger(Object... parameters) {
+	}
+
+}
--- core/src/main/java/com/gemserk/componentsengine/triggers/Trigger.java
+++ core/src/main/java/com/gemserk/componentsengine/triggers/Trigger.java
@@ -1,6 +1,9 @@
 package com.gemserk.componentsengine.triggers;
 
 public interface Trigger {
+	
 	void trigger();
+
 	void trigger(Object... parameters);
+	
 }
