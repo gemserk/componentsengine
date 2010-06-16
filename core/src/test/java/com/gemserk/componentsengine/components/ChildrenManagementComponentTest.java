ab2a1fcb579198e00b73c48a19758d55f9ba64c8
Ariel Coppes <ariel.coppes@gemserk.com>
2010-06-16 22:36:34 +0000

    fixed ChildrenManagementComponent tests and added test when the root entity is the target(where) entity  in the add entity message.
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@143 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- core/src/test/java/com/gemserk/componentsengine/components/ChildrenManagementComponentTest.java
+++ core/src/test/java/com/gemserk/componentsengine/components/ChildrenManagementComponentTest.java
@@ -1,32 +1,34 @@
 package com.gemserk.componentsengine.components;
 
-import org.jmock.*;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.gemserk.componentsengine.entities.Entity;
-import com.gemserk.componentsengine.messages.*;
+import com.gemserk.componentsengine.messages.AddEntityMessage;
+import com.gemserk.componentsengine.messages.RemoveEntityMessage;
 
 @RunWith(JMock.class)
 public class ChildrenManagementComponentTest {
-	
+
 	Mockery mockery = new Mockery() {
 		{
 			setImposteriser(ClassImposteriser.INSTANCE);
 		}
 	};
-	
+
 	@Test
 	public void shouldRemoveEntityFromEntityWhenRemoveEntityMessage() {
 		final Entity rootEntity = mockery.mock(Entity.class, "rootEntity");
 		final Entity childEntity = mockery.mock(Entity.class, "childEntity");
 		final String childName = "childName";
-		
+
 		ChildrenManagementComponent childrenManagementComponent = new ChildrenManagementComponent("");
 		childrenManagementComponent.setEntity(rootEntity);
-		
+
 		mockery.checking(new Expectations() {
 			{
 				oneOf(rootEntity).getEntityById(childName);
@@ -34,68 +36,91 @@ public class ChildrenManagementComponentTest {
 				oneOf(childEntity).removeFromParent();
 			}
 		});
-		
+
 		childrenManagementComponent.handleMessage(new RemoveEntityMessage(childName));
 	}
-	
+
 	@Test
 	public void shouldNotToRemoveWhenChildNotFound() {
 		final Entity rootEntity = mockery.mock(Entity.class, "rootEntity");
 		final String childName = "childName";
-		
+
 		ChildrenManagementComponent childrenManagementComponent = new ChildrenManagementComponent("");
 		childrenManagementComponent.setEntity(rootEntity);
-		
+
 		mockery.checking(new Expectations() {
 			{
 				oneOf(rootEntity).getEntityById(childName);
 				will(returnValue(null));
 			}
 		});
-		
+
 		childrenManagementComponent.handleMessage(new RemoveEntityMessage(childName));
 	}
-	
+
 	// test: remove rootEntity? root entity doesn't have root entity as child!!
-	
+
 	@Test
 	public void shouldAddEntityToEntityWhenAddEntityMessage() {
 		final Entity rootEntity = mockery.mock(Entity.class, "rootEntityMock");
 		final Entity childEntity = mockery.mock(Entity.class, "childEntityMock");
 		final Entity entity = new Entity("");
 		final String whereEntityId = "targetEntity";
-		
+
 		ChildrenManagementComponent childrenManagementComponent = new ChildrenManagementComponent("");
 		childrenManagementComponent.setEntity(rootEntity);
-		
+
 		mockery.checking(new Expectations() {
 			{
+				oneOf(rootEntity).getId();
+				will(returnValue("root"));
 				oneOf(rootEntity).getEntityById(whereEntityId);
 				will(returnValue(childEntity));
 				oneOf(childEntity).addEntity(entity);
 			}
 		});
-		
+
 		childrenManagementComponent.handleMessage(new AddEntityMessage(entity, whereEntityId));
 	}
-	
+
+	@Test
+	public void shouldReturnOwnerEntityWhenItIsTheExpectedWhereEntity() {
+		final Entity rootEntity = mockery.mock(Entity.class, "rootEntityMock");
+		final Entity entity = new Entity("");
+
+		ChildrenManagementComponent childrenManagementComponent = new ChildrenManagementComponent("");
+		childrenManagementComponent.setEntity(rootEntity);
+
+		mockery.checking(new Expectations() {
+			{
+				oneOf(rootEntity).getId();
+				will(returnValue("root"));
+				oneOf(rootEntity).addEntity(entity);
+			}
+		});
+
+		childrenManagementComponent.handleMessage(new AddEntityMessage(entity, "root"));
+	}
+
 	@Test
 	public void shouldNotToAddEntityWhenTargetEntityIsNull() {
 		final Entity rootEntity = mockery.mock(Entity.class, "rootEntityMock");
 		final Entity entity = new Entity("");
 		final String targetEntityId = "targetEntity";
-		
+
 		ChildrenManagementComponent childrenManagementComponent = new ChildrenManagementComponent("");
 		childrenManagementComponent.setEntity(rootEntity);
-		
+
 		mockery.checking(new Expectations() {
 			{
+				oneOf(rootEntity).getId();
+				will(returnValue("root"));
 				oneOf(rootEntity).getEntityById(targetEntityId);
 				will(returnValue(null));
 			}
 		});
-		
+
 		childrenManagementComponent.handleMessage(new AddEntityMessage(entity, targetEntityId));
 	}
-	
+
 }
