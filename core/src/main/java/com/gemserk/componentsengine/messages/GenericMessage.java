f0d5d649ef2e7d04c975f2669a7560bc934d5005
Ruben Garat <ruben.garat@gemserk.com>
2010-06-21 18:28:54 +0000

    starting to remove custom messages in favor of id for everyone
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@146 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- core/src/main/java/com/gemserk/componentsengine/messages/GenericMessage.java
+++ core/src/main/java/com/gemserk/componentsengine/messages/GenericMessage.java
@@ -1,46 +1,18 @@
 package com.gemserk.componentsengine.messages;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.gemserk.componentsengine.properties.PropertiesHolder;
-import com.gemserk.componentsengine.properties.PropertiesHolderImpl;
 import com.gemserk.componentsengine.properties.Property;
 
 public class GenericMessage extends Message implements PropertiesHolder {
 
-	String id;
-
-	PropertiesHolder propertiesHolder = new PropertiesHolderImpl();
-
-	public String getId() {
-		return id;
-	}
-
 	public GenericMessage(String id) {
-		this(id, new HashMap<String, Property<Object>>());
+		super(id);
 	}
 
 	public GenericMessage(String id, Map<String, Property<Object>> properties) {
-		this.id = id;
-		this.propertiesHolder = new PropertiesHolderImpl(properties);
-	}
-
-	public void addProperty(String key, Property<Object> value) {
-		propertiesHolder.addProperty(key, value);
-	}
-
-	public Map<String, Property<Object>> getProperties() {
-		return propertiesHolder.getProperties();
-	}
-
-	public Property<Object> getProperty(String key) {
-		return propertiesHolder.getProperty(key);
-	}
-	
-	@Override
-	public String toString() {
-		return "GenericMessage: " + getId();
+		super(id,properties);
 	}
 
 
--- core/src/main/java/com/gemserk/componentsengine/messages/Message.java
+++ core/src/main/java/com/gemserk/componentsengine/messages/Message.java
@@ -1,10 +1,33 @@
 package com.gemserk.componentsengine.messages;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.gemserk.componentsengine.properties.PropertiesHolder;
+import com.gemserk.componentsengine.properties.PropertiesHolderImpl;
+import com.gemserk.componentsengine.properties.Property;
+
 
 public class Message {
 	
 	boolean propagate = true;
 	boolean process = true;
+	protected String id;
+	protected PropertiesHolder propertiesHolder = new PropertiesHolderImpl();
+	
+	public Message(String id) {
+		this(id, new HashMap<String, Property<Object>>());
+	}
+
+	public Message(String id, Map<String, Property<Object>> properties) {
+		this.id = id;
+		this.propertiesHolder = new PropertiesHolderImpl(properties);
+	}
+	
+	public Message() {
+		this(Message.class.getName());
+	}
+	
 	
 	public void suspendPropagation(){
 		propagate = false;
@@ -26,6 +49,27 @@ public class Message {
 	}
 	public boolean shouldProcess() {
 		return process;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void addProperty(String key, Property<Object> value) {
+		propertiesHolder.addProperty(key, value);
+	}
+
+	public Map<String, Property<Object>> getProperties() {
+		return propertiesHolder.getProperties();
+	}
+
+	public Property<Object> getProperty(String key) {
+		return propertiesHolder.getProperty(key);
+	}
+
+	@Override
+	public String toString() {
+		return "GenericMessage: " + getId();
 	}
 	
 }
