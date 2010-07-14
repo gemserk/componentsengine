74d01430822b18a1258cbc525ca13e5e97b8ccc3
Ariel Coppes <ariel.coppes@gemserk.com>
2010-07-14 19:03:24 +0000

    now there is possible to set the id when using an InstantiationTemplate using withId(id) method.
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@176 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- core/src/main/java/com/gemserk/componentsengine/instantiationtemplates/InstantiationTemplate.java
+++ core/src/main/java/com/gemserk/componentsengine/instantiationtemplates/InstantiationTemplate.java
@@ -8,4 +8,10 @@ public interface InstantiationTemplate {
 
 	Entity get(Object... parameters);
 
+	/**
+	 * @param id used to new instantiated entities 
+	 * @return an IntstantiationTemplate which instantiates entities with specified id
+	 */
+	InstantiationTemplate withId(String id);
+	
 }
--- core/src/main/java/com/gemserk/componentsengine/instantiationtemplates/InstantiationTemplateImpl.java
+++ core/src/main/java/com/gemserk/componentsengine/instantiationtemplates/InstantiationTemplateImpl.java
@@ -8,21 +8,31 @@ import com.gemserk.componentsengine.templates.EntityTemplate;
 
 public class InstantiationTemplateImpl implements InstantiationTemplate {
 
+	String id;
 	EntityTemplate template;
-
 	GenericProvider genericProvider;
 
 	public InstantiationTemplateImpl(EntityTemplate template, GenericProvider genericProvider) {
-		this.template = template;
-		this.genericProvider = genericProvider;
+		this("", template, genericProvider);
 	}
 
+	private InstantiationTemplateImpl(String id, EntityTemplate template, GenericProvider genericProvider) {
+		this.id = id;
+		this.genericProvider = genericProvider;
+		this.template = template;
+	}
+	
 	public Entity get(){
 		return get(new Object[]{});
 	}
 	
 	public Entity get(Object ...parameters){
 		Map<String, Object> entityParameters = genericProvider.get(parameters);
-		return template.instantiate("", entityParameters); //TODO: review id parameter
+		return template.instantiate(id, entityParameters); 
 	}
+	
+	public InstantiationTemplate withId(String id) {
+		return new InstantiationTemplateImpl(id, template, genericProvider);
+	}
+	
 }
