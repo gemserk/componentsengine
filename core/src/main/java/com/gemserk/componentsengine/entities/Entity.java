4900b912ad2676067beb4f27cb5a3bd49313cd6c
Ariel Coppes <ariel.coppes@gemserk.com>
2010-08-23 01:51:48 +0000

    added getParent as public method
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@183 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- core/src/main/java/com/gemserk/componentsengine/entities/Entity.java
+++ core/src/main/java/com/gemserk/componentsengine/entities/Entity.java
@@ -1,13 +1,25 @@
 package com.gemserk.componentsengine.entities;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.Map.Entry;
 
-import com.gemserk.componentsengine.components.*;
-import com.gemserk.componentsengine.messages.Message;
-import com.gemserk.componentsengine.properties.*;
+import com.gemserk.componentsengine.components.Component;
+import com.gemserk.componentsengine.components.ComponentsHolder;
+import com.gemserk.componentsengine.components.ComponentsHolderImpl;
+import com.gemserk.componentsengine.properties.PropertiesHolder;
+import com.gemserk.componentsengine.properties.PropertiesHolderImpl;
+import com.gemserk.componentsengine.properties.Property;
 import com.google.common.base.Predicate;
-import com.google.common.collect.*;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
 
 public class Entity implements PropertiesHolder, ComponentsHolder {
 
@@ -31,6 +43,10 @@ public class Entity implements PropertiesHolder, ComponentsHolder {
 
 	public String getId() {
 		return id;
+	}
+	
+	public Entity getParent() {
+		return parent;
 	}
 
 	public Map<String, Entity> getChildren() {
