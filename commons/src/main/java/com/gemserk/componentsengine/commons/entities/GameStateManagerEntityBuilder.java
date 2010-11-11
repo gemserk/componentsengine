2529afabd0f27f90a351122d6ab8911bc71546a4
Ariel Coppes <ariel.coppes@gemserk.com>
2010-11-11 19:25:29 +0000

    added GameStateManager entity template for an entity with game state manager behavior.
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@255 69e4d4fc-dda9-4998-a343-e80dfd0be058

---
+++ commons/src/main/java/com/gemserk/componentsengine/commons/entities/GameStateManagerEntityBuilder.java
@@ -0,0 +1,104 @@
+package com.gemserk.componentsengine.commons.entities;
+
+import java.util.Map;
+
+import com.gemserk.componentsengine.components.Component;
+import com.gemserk.componentsengine.components.FieldsReflectionComponent;
+import com.gemserk.componentsengine.components.annotations.EntityProperty;
+import com.gemserk.componentsengine.components.annotations.Handles;
+import com.gemserk.componentsengine.entities.Entity;
+import com.gemserk.componentsengine.messages.ChildrenManagementMessageFactory;
+import com.gemserk.componentsengine.messages.Message;
+import com.gemserk.componentsengine.messages.MessageQueue;
+import com.gemserk.componentsengine.properties.Properties;
+import com.gemserk.componentsengine.properties.PropertiesMapBuilder;
+import com.gemserk.componentsengine.templates.EntityBuilder;
+import com.google.inject.Inject;
+
+public class GameStateManagerEntityBuilder extends EntityBuilder {
+
+	static class HandleStateChangedComponent extends FieldsReflectionComponent {
+		
+		@Inject
+		MessageQueue messageQueue;
+		
+		@EntityProperty
+		Entity currentState;
+
+		HandleStateChangedComponent(String id) {
+			super(id);
+		}
+
+		@Handles
+		public void changeNodeState(Message message) {
+
+			Entity newState = Properties.getValue(message, "state");
+
+			if (newState == currentState)
+				return;
+
+			if (currentState != null)
+				messageQueue.enqueue(ChildrenManagementMessageFactory.removeEntity(currentState));
+
+			messageQueue.enqueue(ChildrenManagementMessageFactory.addEntity(newState, entity));
+
+			currentState = newState;
+		}
+	}
+
+	static class HandleStateTransitionComponent extends Component {
+
+		@Inject
+		MessageQueue messageQueue;
+
+		HandleStateTransitionComponent(String id) {
+			super(id);
+		}
+
+		@Override
+		public void handleMessage(Message message) {
+
+			Map<String, String> transitions = Properties.getValue(entity, getId() + ".transitions");
+
+			if (!transitions.containsKey(message.getId()))
+				return;
+
+			String transition = transitions.get(message.getId());
+
+			Map<String, Entity> stateEntities = Properties.getValue(entity, getId() + ".stateEntities");
+
+			Entity stateEntity = stateEntities.get(transition);
+
+			messageQueue.enqueueDelay(new Message("leaveNodeState", new PropertiesMapBuilder().property("message", message).build()));
+			messageQueue.enqueueDelay(new Message("changeNodeState", new PropertiesMapBuilder().property("state", stateEntity).build()));
+			messageQueue.enqueueDelay(new Message("enterNodeState", new PropertiesMapBuilder().property("message", message).build()));
+		}
+	}
+
+	@Override
+	public void build() {
+
+		tags("gameStateManager");
+
+		property("transitions", parameters.get("transitions"));
+
+		property("stateEntities", parameters.get("stateEntities"));
+
+		property("currentState", null);
+
+		component(new HandleStateTransitionComponent("handleTransitionComponent")).withProperties(new ComponentProperties() {
+			{
+				propertyRef("transitions", "transitions");
+				propertyRef("stateEntities", "stateEntities");
+			}
+		});
+
+		component(new HandleStateChangedComponent("handleChangeStateComponent")).withProperties(new ComponentProperties() {
+			{
+				propertyRef("currentState", "currentState");
+			}
+		});
+
+	}
+
+}
