d659a8db36c2f2b9009336e3a18cb11cb9113bf3
Ariel Coppes <ariel.coppes@gemserk.com>
2010-11-18 21:04:26 +0000

    added GlobalProperties
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@262 69e4d4fc-dda9-4998-a343-e80dfd0be058

---
+++ core/src/main/java/com/gemserk/componentsengine/game/GlobalProperties.java
@@ -0,0 +1,14 @@
+package com.gemserk.componentsengine.game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalProperties {
+	
+	Map<String, Object> properties = new HashMap<String, Object>();
+	
+	public Map<String, Object> getProperties() {
+		return properties;
+	}
+	
+}
