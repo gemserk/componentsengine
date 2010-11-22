a528379baadca8025cf1827624e02331f49b03ad
Ariel Coppes <ariel.coppes@gemserk.com>
2010-11-22 15:58:45 +0000

    added a method getDistanceToNextPoint to return the length of a specified path inner segment, and added a cache for those distances.
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@265 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- commons/src/main/java/com/gemserk/componentsengine/commons/components/Path.java
+++ commons/src/main/java/com/gemserk/componentsengine/commons/components/Path.java
@@ -6,14 +6,25 @@ import org.newdawn.slick.geom.Vector2f;
 
 public class Path {
 
-	private List<Vector2f> points ;
+	private List<Vector2f> points;
+
+	private Float[] segmentDistances = null;
 
 	public List<Vector2f> getPoints() {
 		return points;
 	}
-	
+
 	public Path(List<Vector2f> points) {
 		this.points = points;
+		if (points.size() == 0)
+			return;
+		recalculateDistances(points);
+	}
+
+	private void recalculateDistances(List<Vector2f> points) {
+		segmentDistances = new Float[points.size()];
+		for (int i = 0; i < points.size(); i++)
+			segmentDistances[i] = getSegmentDistance(i);
 	}
 
 	public int getNextIndex(Vector2f position, int index) {
@@ -27,4 +38,26 @@ public class Path {
 		return points.get(index);
 	}
 
+	public float getDistanceToNextPoint(int index) {
+		if (segmentDistances == null || segmentDistances.length != points.size()) {
+			if (points.size() == 0 || points.size() == 1)
+				return 0f;
+			recalculateDistances(getPoints());
+		}
+		return segmentDistances[index];
+	}
+
+	private float getSegmentDistance(int index) {
+		if (isLastSegment(index))
+			return 0f;
+
+		Vector2f p0 = getPoint(index);
+		Vector2f p1 = getPoint(index + 1);
+
+		return p0.distance(p1);
+	}
+
+	public boolean isLastSegment(int index) {
+		return index == getPoints().size() - 1;
+	}
 }
