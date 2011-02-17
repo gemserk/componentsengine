66117d7c58396eff627c99935070ef0a57061eb1
Ariel Coppes <ariel.coppes@gemserk.com>
2011-02-17 17:27:58 +0000

    changed the LinkButton entity template to send message onCursorEnter when onEnterTrigger is triggered and onCursorLeave when onLeaveTrigger is triggered.
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@298 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- commons/src/main/java/com/gemserk/componentsengine/commons/entities/gui/LinkButtonEntityBuilder.java
+++ commons/src/main/java/com/gemserk/componentsengine/commons/entities/gui/LinkButtonEntityBuilder.java
@@ -5,12 +5,22 @@ import org.newdawn.slick.geom.Vector2f;
 import com.gemserk.componentsengine.commons.components.CursorOverDetector;
 import com.gemserk.componentsengine.commons.components.SlickMouseHandlerComponent;
 import com.gemserk.componentsengine.commons.components.SlickMouseMoveHandlerComponent;
+import com.gemserk.componentsengine.entities.Entity;
+import com.gemserk.componentsengine.messages.MessageQueue;
+import com.gemserk.componentsengine.messages.messageBuilder.MessageBuilder;
 import com.gemserk.componentsengine.templates.EntityBuilder;
 import com.gemserk.componentsengine.triggers.NullTrigger;
 import com.gemserk.componentsengine.triggers.OpenLinkTrigger;
+import com.google.inject.Inject;
 
 public class LinkButtonEntityBuilder extends EntityBuilder {
 
+	@Inject
+	MessageQueue messageQueue;
+
+	@Inject
+	MessageBuilder messageBuilder;
+	
 	@Override
 	public void build() {
 
@@ -26,8 +36,18 @@ public class LinkButtonEntityBuilder extends EntityBuilder {
 		component(new CursorOverDetector("cursorOver")).withProperties(new ComponentProperties() {
 			{
 				property("bounds", parameters.get("bounds"));
-				property("onEnterTrigger", parameters.get("onEnterTrigger") != null ? parameters.get("onEnterTrigger") : new NullTrigger());
-				property("onLeaveTrigger", parameters.get("onLeaveTrigger") != null ? parameters.get("onLeaveTrigger") : new NullTrigger());
+				property("onEnterTrigger", parameters.get("onEnterTrigger") != null ? parameters.get("onEnterTrigger") : new NullTrigger() {
+					@Override
+					public void trigger(Object... parameters) {
+						messageQueue.enqueue(messageBuilder.newMessage("onCursorEnter").property("source", (Entity) parameters[0]).get());
+					}
+				});
+				property("onLeaveTrigger", parameters.get("onLeaveTrigger") != null ? parameters.get("onLeaveTrigger") : new NullTrigger() {
+					@Override
+					public void trigger(Object... parameters) {
+						messageQueue.enqueue(messageBuilder.newMessage("onCursorLeave").property("source", (Entity) parameters[0]).get());
+					}
+				});
 			}
 		});
 
