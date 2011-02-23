2b16e57426c3fa23de48466002cd630918018005
Ariel Coppes <ariel.coppes@gemserk.com>
2011-02-23 20:02:10 +0000

    added the Java2dMonitorFactory and configured the Java2dModule for injection.
    git-svn-id: https://svn.gemserk.com/componentsengine/trunk/componentsengine@303 69e4d4fc-dda9-4998-a343-e80dfd0be058

--- core/src/main/java/com/gemserk/componentsengine/java2d/Java2dModule.java
+++ core/src/main/java/com/gemserk/componentsengine/java2d/Java2dModule.java
@@ -1,5 +1,9 @@
 package com.gemserk.componentsengine.java2d;
 
+import com.gemserk.componentsengine.input.CachedMonitorFactory;
+import com.gemserk.componentsengine.input.MonitorFactory;
+import com.gemserk.componentsengine.input.MonitorUpdater;
+import com.gemserk.componentsengine.java2d.input.Java2dMonitorFactory;
 import com.gemserk.componentsengine.java2d.render.CurrentGraphicsProvider;
 import com.gemserk.componentsengine.resources.images.ImageLoader;
 import com.google.inject.AbstractModule;
@@ -19,6 +23,13 @@ public class Java2dModule extends AbstractModule {
 		bind(ImageLoader.class).to(Java2dImageLoader.class).in(Singleton.class);
 		bind(KeyboardInput.class).toInstance(java2dWindow.getKeyboardInput());
 		bind(MouseInput.class).toInstance(java2dWindow.getMouseInput());
+		
+		MonitorFactory monitorFactory = new Java2dMonitorFactory();
+		requestInjection(monitorFactory);
+		CachedMonitorFactory cachedMonitorFactory = new CachedMonitorFactory(monitorFactory);
+		bind(MonitorFactory.class).toInstance(cachedMonitorFactory);
+		bind(MonitorUpdater.class).toInstance(cachedMonitorFactory);
+
 	}
 	
 }
---
+++ core/src/main/java/com/gemserk/componentsengine/java2d/input/Java2dMonitorFactory.java
@@ -0,0 +1,102 @@
+package com.gemserk.componentsengine.java2d.input;
+
+import java.awt.event.MouseEvent;
+
+import com.gemserk.componentsengine.input.ButtonMonitor;
+import com.gemserk.componentsengine.input.CoordinatesMonitor;
+import com.gemserk.componentsengine.input.MonitorFactory;
+import com.gemserk.componentsengine.java2d.KeyboardInput;
+import com.gemserk.componentsengine.java2d.MouseInput;
+import com.google.inject.Inject;
+
+public class Java2dMonitorFactory implements MonitorFactory {
+	
+	@Inject
+	KeyboardInput keyboardInput;
+	
+	@Inject
+	MouseInput mouseInput;
+	
+	@Inject
+	KeyEventCodeMapping keyEventCodeMapping;
+
+	@Override
+	public ButtonMonitor keyboardButtonMonitor(final String button) {
+		return new ButtonMonitor() {
+			
+			@Override
+			protected boolean isDown() {
+				return keyboardInput.keyDown(keyEventCodeMapping.getKeyCode(button));
+			}
+
+		};
+	}
+
+	@Override
+	public ButtonMonitor mouseButtonMonitor(final String button) {
+
+		return new ButtonMonitor() {
+			
+			@Override
+			protected boolean isDown() {
+				return mouseInput.buttonDown(getButtonValue(button));
+			}
+
+			// TODO: use a map
+			private int getButtonValue(String button) {
+				if ("left".equalsIgnoreCase(button))
+					return MouseEvent.BUTTON1;
+				if ("mid".equalsIgnoreCase(button))
+					return MouseEvent.BUTTON2;
+				if ("right".equalsIgnoreCase(button))
+					return MouseEvent.BUTTON3;
+				return MouseEvent.NOBUTTON;
+			}
+			
+		};
+		
+	}
+
+	@Override
+	public CoordinatesMonitor mouseCoordinatesMonitor() {
+		return new CoordinatesMonitor() {
+			
+			@Override
+			protected float readY() {
+				return (float) mouseInput.getPosition().getX();
+			}
+			
+			@Override
+			protected float readX() {
+				return (float) mouseInput.getPosition().getY();
+			}
+		};
+	}
+
+	@Override
+	public CoordinatesMonitor mouseWheelMonitor() {
+
+		return new CoordinatesMonitor() {
+			
+			@Override
+			protected float readX() {
+				return 0;
+			}
+			
+			@Override
+			protected float readY() {
+				// It would be better to call it using a slick api, but there isn't a method on Input class.
+				// return MouseInput.getDWheel();
+				return mouseInput.getWheelRotation();
+			}
+			
+			@Override
+			public boolean hasChanged() {
+				return getY() != 0f;
+			}
+			
+		};
+		
+	}
+	
+}
