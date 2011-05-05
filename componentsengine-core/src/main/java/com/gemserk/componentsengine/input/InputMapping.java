6489fa704fa8ceb29b4801859d451ee04281c0b3
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-05 12:01:57 -0300

    a bit of file formatting

--- componentsengine-core/src/main/java/com/gemserk/componentsengine/input/InputMapping.java
+++ componentsengine-core/src/main/java/com/gemserk/componentsengine/input/InputMapping.java
@@ -6,23 +6,22 @@ import java.util.List;
 import com.gemserk.componentsengine.messages.Message;
 
 public class InputMapping {
-	
-	List<InputAction> inputActions = new ArrayList<InputAction>();
+
+	private List<InputAction> inputActions = new ArrayList<InputAction>();
 
 	public void addAction(InputAction inputAction) {
 		inputActions.add(inputAction);
 	}
 
 	public List<Message> update() {
-			
-			List<Message> messages = new ArrayList<Message>(inputActions.size());
-			for (InputAction inputAction : inputActions) {
-				Message newMessage = inputAction.run();
-				if (newMessage != null) {
-					messages.add(newMessage);
-				}
+		List<Message> messages = new ArrayList<Message>(inputActions.size());
+		for (InputAction inputAction : inputActions) {
+			Message newMessage = inputAction.run();
+			if (newMessage != null) {
+				messages.add(newMessage);
 			}
-			return messages;
+		}
+		return messages;
 	}
 
 }
