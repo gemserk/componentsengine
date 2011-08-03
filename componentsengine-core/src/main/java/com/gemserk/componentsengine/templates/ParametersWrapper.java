1ea4fefd714169b35b129005b5d9266aae4047c7
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-03 13:25:47 -0300

    added a bit of javadocs to ParametersWrapper class

--- componentsengine-core/src/main/java/com/gemserk/componentsengine/templates/ParametersWrapper.java
+++ componentsengine-core/src/main/java/com/gemserk/componentsengine/templates/ParametersWrapper.java
@@ -3,25 +3,52 @@ package com.gemserk.componentsengine.templates;
 import java.util.Map;
 
 /**
- * Helper class to access parameters when using an entity builder.
+ * Lets you work with a map<string, object> in an easy way, using generic methods.
  */
 public class ParametersWrapper {
 
 	private Map<String, Object> wrappedParameters;
-	
+
+	/**
+	 * Sets the map from where the properties will be extracted.
+	 * 
+	 * @param wrappedParameters
+	 *            The new map with properties to be extracted.
+	 */
 	public void setWrappedParameters(Map<String, Object> wrappedParameters) {
 		this.wrappedParameters = wrappedParameters;
 	}
-	
+
+	/**
+	 * Returns the original map from where the properties are being extracted.
+	 */
 	public Map<String, Object> getWrappedParameters() {
 		return wrappedParameters;
 	}
 
+	/**
+	 * Returns the object from the map identified by id auto casting it to specified type.
+	 * 
+	 * @param <T>
+	 *            The type of the returned object.
+	 * @param id
+	 *            The identifier of the object in the map.
+	 */
 	@SuppressWarnings("unchecked")
 	public <T> T get(String id) {
 		return (T) wrappedParameters.get(id);
 	}
 
+	/**
+	 * Returns the object from the map identified by id auto casting it to specified type.
+	 * 
+	 * @param <T>
+	 *            The type of the returned object.
+	 * @param id
+	 *            The identifier of the object in the map.
+	 * @param defaultValue
+	 *            The default value of the returned object if the map doesn't contains any object identified by id.
+	 */
 	@SuppressWarnings("unchecked")
 	public <T> T get(String id, T defaultValue) {
 		if (!wrappedParameters.containsKey(id))
@@ -29,8 +56,16 @@ public class ParametersWrapper {
 		return (T) wrappedParameters.get(id);
 	}
 
-	public void put(String key, Object value) {
-		wrappedParameters.put(key, value);
+	/**
+	 * Puts a new object in the map.
+	 * 
+	 * @param id
+	 *            The identifier of the object.
+	 * @param value
+	 *            The object to add to the map.
+	 */
+	public void put(String id, Object value) {
+		wrappedParameters.put(id, value);
 	}
 
 }
